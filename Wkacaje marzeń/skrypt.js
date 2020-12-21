08719314f139c5bad94d955701e7a28298bbf148
jakubwojcik987 <58569678+jakubwojcik987@users.noreply.github.com>
2020-12-21 09:42:50 +0100

    Add files via upload

---
+++ Wkacaje marzeń/skrypt.js
@@ -0,0 +1,10 @@
+function skrypt(){
+  goralewy.style.backgroundColor='white';
+  goraprawy.style.backgroundColor='white';
+  baner.style.backgroundColor='white';
+  glownylewy.style.backgroundColor='white';
+  glownyprawy.style.backgroundColor='white';
+  footer.style.backgroundColor='white';
+  nagl.style.color='black';
+  naglowek.style.color='black';
+}
